0d1790d07f16549b1d7f86eb94bb401f8c0c372b
Mauricio Malsenido <mauriciomalsenido@gmail.com>
2022-01-07 20:52:10 -0300

    se quitaron los parentesis en el ejercicio 6 porque estaban de mas

--- Tema 3 - Queries/Ejercicio 6.sql
+++ Tema 3 - Queries/Ejercicio 6.sql
@@ -1,3 +1,3 @@
 -- Los profesores que nacieron en la década del 80 y tienen un salario mayor a 80000
 
-SELECT * FROM profesor WHERE (fecha_nacimiento BETWEEN '1980-01-01' AND '1989-12-31') AND salario > 80000
+SELECT * FROM profesor WHERE fecha_nacimiento BETWEEN '1980-01-01' AND '1989-12-31' AND salario > 80000
